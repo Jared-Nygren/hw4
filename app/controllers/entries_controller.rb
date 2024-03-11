f869d0c138018c8c49f009f65da38abea1d5f022
Jared Nygren <jared.nygren@kellogg.northwestern.edu>
2024-03-11 23:31:12 +0000

    Updated descriptions in code

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,45 +1,60 @@
 class EntriesController < ApplicationController
+  
+  # The index action displays a list of entries to the user.
   def index
+    # Check if the user is logged in by seeing if their user_id is stored in the session.
     if session["user_id"].nil?
+      # If not logged in, show a notice and redirect them to the login page.
       flash["notice"] = "You must be logged in to view entries"
       redirect_to "/login"
     else
+      # If logged in, find all entries belonging to the user and store them in a variable.
       @entries = Entry.where(user_id: session["user_id"])
     end
   end
 
+  # The new action sets up the form for adding a new entry.
   def new
+    # Again, check if the user is logged in.
     if session["user_id"].nil?
+      # If not, notify and redirect to login.
       flash["notice"] = "You must be logged in to add a new entry"
       redirect_to "/login"
     else
+      # If logged in, prepare a new Entry object and store the place_id from the request parameters.
       @place_id = params[:place_id]
       @entry = Entry.new
     end
   end
 
+  # The create action processes the form submission to add a new entry.
   def create
+    # Check for user login status.
     if session["user_id"].nil?
+      # Notify and redirect to login if not logged in.
       flash["notice"] = "You must be logged in to add a new entry"
       redirect_to "/login"
     else
+      # If logged in, create a new Entry object with parameters from the form, adding the user_id from the session.
       @entry = Entry.new(entry_params.merge(user_id: session["user_id"]))
       
-      # Attach the uploaded image if present
+      # If an image was uploaded with the entry, attach it to the entry.
       if params[:entry][:uploaded_image].present?
         @entry.uploaded_image.attach(params[:entry][:uploaded_image])
       end
       
-      # Ensure place_id is present and corresponds to a valid Place
+      # Validate the place_id to ensure it exists and belongs to the current user.
       if @entry.place_id.blank? || !Place.exists?(id: @entry.place_id, user_id: session["user_id"])
         flash.now[:error] = "Invalid or missing place."
-        render :new and return
+        render :new and return # Render the form again if validation fails.
       end
 
+      # Save the entry. If successful, notify the user and redirect to the entry's place page.
       if @entry.save
         flash[:notice] = "Entry successfully created"
-        redirect_to place_path(@entry.place_id) # Redirect to the place show page
+        redirect_to place_path(@entry.place_id) # Redirect to the place's show page.
       else
+        # If saving fails, show the form again with an error message.
         flash.now[:error] = "Failed to create entry"
         render :new
       end
@@ -48,6 +63,9 @@ class EntriesController < ApplicationController
 
   private
 
+  # Added this in with Chat GPT troubleshooting 
+  # This method lists the safe information that can be sent to the website
+  # It's a security feature to prevent malicious data from being submitted.
   def entry_params
     params.require(:entry).permit(:title, :description, :occurred_on, :place_id)
   end
@@ -58,53 +76,4 @@ end
 
 
 
-# class EntriesController < ApplicationController
-#   def index
-#     if session["user_id"].nil?
-#       flash["notice"] = "You must be logged in to view entries"
-#       redirect_to "/login"
-#     else
-#       @entries = Entry.where(user_id: session["user_id"])
-#     end
-#   end
 
-#   def new
-#     if session["user_id"].nil?
-#       flash["notice"] = "You must be logged in to add a new entry"
-#       redirect_to "/login"
-#     else
-#       @place_id = params[:place_id]
-#       @entry = Entry.new
-#     end
-#   end
-
-#   def create
-#     Rails.logger.debug "Received params: #{params.inspect}"
-#     if session["user_id"].nil?
-#       flash["notice"] = "You must be logged in to add a new entry"
-#       redirect_to "/login"
-#     else
-#       @entry = Entry.new({
-#         "title" => params["title"],
-#         "description" => params["description"],
-#         "occurred_on" => params["occurred_on"],
-#         "place_id" => params["place_id"], # Include place_id in the creation
-#         "user_id" => session["user_id"] # Ensuring the entry is associated with the current user
-#       })
-  
-#       # Check if an image was uploaded
-#       if params[:uploaded_image].present?
-#         @entry.uploaded_image.attach(params[:uploaded_image])
-#       end
-  
-#       if @entry.save
-#         Rails.logger.debug "Place ID: #{@entry.place_id}" # Log place_id after entry is saved
-#         flash["notice"] = "Entry successfully created"
-#         redirect_to place_path(@entry.place_id) # Adjust the redirect to show the place
-#       else
-#         flash.now["error"] = "Failed to create entry"
-#         render :new
-#       end
-#     end
-#   end   
-# end  
